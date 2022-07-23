c39b3b35835d9c8fbde1b0c62f558faaee517e34
myatthu000 <september.2019thu@gmail.com>
2022-07-23 13:29:43 +0630

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <EventCard
+            msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    EventCard,
+  },
+};
+</script>
