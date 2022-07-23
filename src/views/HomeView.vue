a51a1f97797d421a57eacc76f4062f91678926ca
myatthu000 <september.2019thu@gmail.com>
2022-07-23 13:54:30 +0630

    L3-end

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <EventCard
-            msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h1>Events for Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
@@ -15,5 +14,52 @@ export default {
   components: {
     EventCard,
   },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 8015964,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 1206984,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
