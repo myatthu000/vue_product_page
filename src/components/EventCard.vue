b41f9f9441a7b1ef71e64514f29192d55ffc728a
myatthu000 <september.2019thu@gmail.com>
2022-07-23 13:46:17 +0630

    l3

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="hello"></div>
+  <div class="event-card">
+    <span>@ {{ event.title }} {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
 </template>
 
 <script>
 export default {
   name: "EventCard",
-  // props: {
-  //   msg: String,
-  // },
+  props: {
+    event: Object,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
