d607b79822ad1c20e7bf38da13532f177d362b4f
myatthu000 <september.2019thu@gmail.com>
2022-07-24 10:57:22 +0630

    L3-end

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import EventDetail from "../views/EventDetail.vue";
+import AboutView from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +14,15 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/event/123",
+    name: "EventDetail",
+    props: true,
+    component: EventDetail,
+  },
+  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
